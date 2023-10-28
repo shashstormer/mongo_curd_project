b77198d4bf571dfb7bec9a54c8f367480548a09c
shash <shashanka5398@gmail.com>
2023-10-28 12:18:49 +0530

    leaving pydantic to auto validation

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 from fastapi.params import Query
-from pydantic import BaseModel, Field, field_validator, ValidationError
+from pydantic import BaseModel, Field, ValidationError  # , field_validator
+
+
 # from typing import Any, Literal
 
 
@@ -10,7 +12,7 @@ class Validatiors:
         field_name: str = Field(Query(...), title="Field Name",
                                 description="Vame of field using which field is identified")
         field_value: str or int or dict = Field(Query(...), title="Field Value",
-                                 description="Value of field using which field is identified")
+                                                description="Value of field using which field is identified")
 
     # NOTE: this section has been comented to allow none so that we can fetch all records
     # @field_validator('field_value' , mode="before")
@@ -21,42 +23,44 @@ class Validatiors:
 
     class delete(BaseModel):
         field_name: str = Field(..., title="Field Name", description="Name of field using which field is identified")
-        field_value: str or int or dict = Field(..., title="Field Value", description="Value of field using which field is identified")
+        field_value: str or int or dict = Field(..., title="Field Value",
+                                                description="Value of field using which field is identified")
 
-        @staticmethod
-        @field_validator('field_value', mode="before")
-        def notNone(value):
-            if value is None:
-                raise ValidationError('The Field Value Cannot Be None')
-            return value
+        # @staticmethod
+        # @field_validator('field_value', mode="before")
+        # def notNone(value):
+        #     if value is None:
+        #         raise ValidationError('The Field Value Cannot Be None')
+        #     return value
 
     class update(BaseModel):
         field_name: str = Field(..., title="Field Name", description="Name of field using which field is identified")
-        field_value: str or int or dict = Field(..., title="Field Value", description="Value of field using which field is identified")
+        field_value: str or int or dict = Field(..., title="Field Value",
+                                                description="Value of field using which field is identified")
         data: dict = Field(..., title="Data", description="Data to be updated")
 
-        @staticmethod
-        @field_validator('field_value', mode="before")
-        def notNone(value):
-            if value is None:
-                raise ValidationError('The Field Value Cannot Be None')
-            return value
+        # @staticmethod
+        # @field_validator('field_value', mode="before")
+        # def notNone(value):
+        #     if value is None:
+        #         raise ValidationError('The Field Value Cannot Be None')
+        #     return value
 
     class by_id(BaseModel):
         id_: str or int or dict = Field(...)
 
-        @staticmethod
-        @field_validator('id_', mode="before")
-        def notNone(value):
-            if value is None:
-                raise ValidationError('No Data to ADD')
-            return value
+        # @staticmethod
+        # @field_validator('id_', mode="before")
+        # def notNone(value):
+        #     if value is None:
+        #         raise ValidationError('No Data to ADD')
+        #     return value
 
     class create(BaseModel):
         data: str or int or dict = Field(...)
-        @staticmethod
-        @field_validator('data', mode="before")
-        def notNone(value):
-            if value is None:
-                raise ValidationError('No Data to ADD')
-            return value
+        # @staticmethod
+        # @field_validator('data', mode="before")
+        # def notNone(value):
+        #     if value is None:
+        #         raise ValidationError('No Data to ADD')
+        #     return value
